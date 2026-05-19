468804d77a856b216df88ffb48dec9778a8a3d3a
Amol Shinde <112163195+amol03051999@users.noreply.github.com>
2026-05-19 15:41:26 +0530

    Add scrollTo function on NavLink clicks in Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -15,13 +15,13 @@ const Footer = () => {
         <div>
             <p className="text-xl font-medium mb-5">COMPANY</p>
             <ul className="flex flex-col gap-2 text-gray-600">
-              <NavLink to='/'>
+              <NavLink to='/' onClick={()=>scrollTo(0,0)}>
                 <li>Home</li>
               </NavLink>
-              <NavLink to='/about'>
+              <NavLink to='/about' onClick={()=>scrollTo(0,0)}>
                 <li>About us</li>
               </NavLink>
-              <NavLink to='/contact'>
+              <NavLink to='/contact' onClick={()=>scrollTo(0,0)}>
                 <li>Contact us</li>
               </NavLink>
             </ul>
