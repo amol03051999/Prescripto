6b2991ac25b626ebf6f4fe0212688154e07ade9d
Amol Shinde <112163195+amol03051999@users.noreply.github.com>
2026-05-19 15:30:08 +0530

    Remove unused import of useNavigate in Footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { assets } from "../assets/assets";
-import { NavLink, useNavigate } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Footer = () => {
   return (
