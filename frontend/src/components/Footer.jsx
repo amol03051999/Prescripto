3094ff39e55719422b88e407a0192bb271c5170f
Amol Shinde <112163195+amol03051999@users.noreply.github.com>
2026-05-19 15:52:49 +0530

    Refactor Footer component for better scrolling behavior

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,46 +1,81 @@
 import React from "react";
 import { assets } from "../assets/assets";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+
+  const handleScrollTop = () => {
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    }, 0);
+  };
+
   return (
     <div className="md:mx-10">
-      <div className=" flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+      <div className="flex flex-col sm:grid sm:grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+
         {/* left section */}
         <div>
           <img className="mb-5 w-40" src={assets.logo} alt="logo" />
-          <p className="w-full md:w-2/3 text-gray-600 leading-6">lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
+            Lorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s.
+          </p>
         </div>
+
         {/* center section */}
         <div>
-            <p className="text-xl font-medium mb-5">COMPANY</p>
-            <ul className="flex flex-col gap-2 text-gray-600">
-              <Link to='/' onClick={()=>window.scrollTo(0,0)}>
-                <li>Home</li>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+
+          <ul className="flex flex-col gap-2 text-gray-600">
+
+            <li>
+              <Link to="/" onClick={handleScrollTop}>
+                Home
               </Link>
-              <Link to='/about' onClick={()=>window.scrollTo(0,0)}>
-                <li>About us</li>
+            </li>
+
+            <li>
+              <Link to="/about" onClick={handleScrollTop}>
+                About us
               </Link>
-              <Link to='/contact' onClick={()=>window.scrollTo(0,0)}>
-                <li>Contact us</li>
+            </li>
+
+            <li>
+              <Link to="/contact" onClick={handleScrollTop}>
+                Contact us
               </Link>
-            </ul>
+            </li>
+
+          </ul>
         </div>
 
         {/* right section */}
         <div>
-            <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
-            <ul className="flex flex-col gap-2 text-gray-600">
-                <li>+91-123-456-7890</li>
-                <li>amoldev@gmail.com</li>
-            </ul>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>+91-123-456-7890</li>
+            <li>amoldev@gmail.com</li>
+          </ul>
         </div>
+
       </div>
-         {/* comment section */}
-             <div>
-             <hr/>
-               <p className="py-5 text-sm text-center">copyright 2025@ Prescripto - All rights Reserved.</p>
-             </div>
+
+      {/* copyright section */}
+      <div>
+        <hr />
+
+        <p className="py-5 text-sm text-center">
+          Copyright 2025 @ Prescripto - All Rights Reserved.
+        </p>
+      </div>
     </div>
   );
 };
