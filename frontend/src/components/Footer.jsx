8887c11bb6dd47471f27e713095fd35f78c26145
Amol Shinde <112163195+amol03051999@users.noreply.github.com>
2026-05-19 15:29:40 +0530

    Update footer links in Footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -15,12 +15,15 @@ const Footer = () => {
         <div>
             <p className="text-xl font-medium mb-5">COMPANY</p>
             <ul className="flex flex-col gap-2 text-gray-600">
+              <NavLink to='/'>
                 <li>Home</li>
-              <NavLink to='/doctors'>
+              </NavLink>
+              <NavLink to='/about'>
                 <li>About us</li>
               </NavLink>
+              <NavLink to='/contact'>
                 <li>Contact us</li>
-                <li>Privacy policy</li>
+              </NavLink>
             </ul>
         </div>
 
